a5576bb1bb0bee806661fac35fce6c924605dc74
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-10-24 10:40:56 -0600

    LobbyCreateUI

--- Assets/Scripts/UI Scripts/LobbyUI.cs
+++ Assets/Scripts/UI Scripts/LobbyUI.cs
@@ -7,13 +7,15 @@ public class LobbyUI : MonoBehaviour {
 	[SerializeField] Button mainMenuButton;
 	[SerializeField] Button createLobbyButton;
 	[SerializeField] Button quickJoinButton;
+	[SerializeField] LobbyCreateUI lobbyCreateUI;
 
 	private void Awake() {
 		mainMenuButton.onClick.AddListener(() => {
 			Loader.Load(Loader.scenes.MainMenu);
 		});
 		createLobbyButton.onClick.AddListener(() => {
-			GameLobby.Instance.CreateLobby("LobbyName", false);
+			//GameLobby.Instance.CreateLobby("LobbyName", false);
+			lobbyCreateUI.Show();
 		});
 		quickJoinButton.onClick.AddListener(() => {
 			GameLobby.Instance.QuickJoin();
